e6b83e381b07b6bf348c38a8c8a13f1337f12bbf
naveen <naveentejas1109@gmail.com>
2020-08-16 12:06:01 +0530

    More destructing examples.

--- destructuring.js
+++ destructuring.js
@@ -23,3 +23,19 @@ function login({username, password}) {
     console.log('password ', password);
 }
 login({username: 'Axelson', password: 'LoveTTY!'});
+
+
+// Default params
+function login1({username, password="LoveTTY!!!"}) {
+    console.log('username ', username);
+    console.log('password ', password);
+}
+login1({username: 'Lin Dan'});
+
+// default param again
+function tweet({corona="Damn you Corona!"}) {
+    console.log(corona);
+}
+
+tweet({corona: 'Why Corona why?!!'});
+// tweet(); // Gotcha!
